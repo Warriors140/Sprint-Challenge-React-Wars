79f170acb7e867a4a3c950fe3ffc789a644afa33
Randy Warrington <warrington.randy@outlook.com>
2019-06-10 20:04:17 -0400

    added character component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import './StarWars.css';
 
 const Character = props => {
-    return <div className='card'>
-        <p>Name: {props.swChar.name}</p>
-        <p>Height: {props.swChar.height}</p>
-        <p>Gender: {props.swChar.gender}</p>
-        <p>Birth Year: {props.swChar.birth_year}</p>
-    </div>
+    return (
+        <div className = 'character'>
+            <h1>{props.character.name}</h1>
+            <div className = 'characterDetails'>
+                <div>Birthday: {props.character.birth_year}</div>
+                <div>Gender: {props.character.gender}</div>
+                <div>Height: {props.character.height}</div>
+                <div>Mass: {props.character.mass}</div>
+                <div>Hair Color: {props.character.hair_color}</div>
+                <div>Skin Color: {props.character.skin_color}</div>
+                
+            </div>
+        </div>
+    )
 }
 
 export default Character;
